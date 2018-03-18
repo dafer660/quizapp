0c551abb240acd8901bc560ff88a36fedb796250
Daniel Ferreira <danielferr18@gmail.com>
2018-03-18 15:52:33 +0000

    some modifications

--- app/src/main/java/com/dafer/android/quizapp/MainActivity.java
+++ app/src/main/java/com/dafer/android/quizapp/MainActivity.java
@@ -1,8 +1,8 @@
 package com.dafer.android.quizapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,4 +17,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Quiz.class);
         startActivity(intent);
     }
+    
 }
